d62b2b8874c8fc4b9cc369817c550fdb12515b39
Mame-Penda <mamependasadio1@gmail.com>
2025-01-17 16:43:37 +0100

    fixed the arguments files

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    argc = len(sys.argv)
+    argv = sys.argv
 
-    count = len(sys.argv) - 1
-    if count == 0:
+    if argc == 1:
         print("0 arguments.")
-    elif count == 1:
-        print("1 arguments:")
+    elif argc == 2:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{:d} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{:d}: {:s}".format(i, argv[i]))
