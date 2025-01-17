09545d31ff51254bbb6541e5551657cce4934e8c
Mame-Penda <mamependasadio1@gmail.com>
2025-01-17 16:41:53 +0100

    fixed the infinite files

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     import sys
     argc = len(sys.argv)
     argv = sys.argv
 
     for i in range(1, argc):
         argv[i] = int(argv[i])
-        print(sum(argv[1:]))
+    print(sum(argv[1:]))
