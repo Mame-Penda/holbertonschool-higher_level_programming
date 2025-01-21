05f5e59ba286b86a264dfd837a31d3103c089df6
Mame-Penda <mamependasadio1@gmail.com>
2025-01-21 17:14:14 +0100

    modified tuples files

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (tuple_a) < 2:
-        if (tuple_a) == 0:
-                tuple_a = 0, 0
-        else:
-                    tuple_a = tuple_a[0], 0
-                    if (tuple_b) < 2:
-                        if (tuple_b) == 0:
-                            tuple_b = 0, 0
-                        else:
-                                tuple_b = tuple_b[0], 0
-                                return (tuple_a[0] + tple_b[0], + tuple_a[1] + tuple_b[1])
+    res1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    res1 += tuple_b[0] if len(tuple_b) > 0 else 0
+
+    res2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    res2 += tuple_b[1] if len(tuple_b) > 1 else 0
+
+    t = (res1, res2)
+    return t
