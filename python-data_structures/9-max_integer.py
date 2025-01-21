ddb2928b82d141eb328509974f287f8952e989e0
Mame-Penda <mamependasadio1@gmail.com>
2025-01-21 21:10:48 +0100

    modified integer files

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (my_list) == []:
+    """
+    Trouve le plus grand entier d'une liste.
+
+    Args:
+        my_list (list): La liste d'entiers.
+
+    Returns:
+        int: Le plus grand entier de la liste, ou None si la liste est vide.
+    """
+    if not my_list:
         return None
-    big = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
-            return big
+
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    return max_value
