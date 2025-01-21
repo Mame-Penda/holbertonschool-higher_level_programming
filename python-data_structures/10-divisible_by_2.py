a8cac38bfee8b3295d9ff342dc62efe539378c11
Mame-Penda <mamependasadio1@gmail.com>
2025-01-21 16:40:30 +0100

    fixed error for divide files

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    multiples = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            multiples.append(True)
-        else:
-            multiples.append(False)
-            return (multiples)
+    if my_list == []:
+         return None
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        new_list[i] = True if new_list[i] % 2 == 0 else False
+        return (new_list)
+
