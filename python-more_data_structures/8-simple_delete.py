e24e9a20b5006a63c584fd7f7eae2d1418cf7f5d
Mame-Penda <mamependasadio1@gmail.com>
2025-01-23 22:41:28 +0100

    fixed error simple delete

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in list(a_dictionary.keys()):
-        if i == key:
-            del a_dictionary[i]
-            return a_dictionary
-            
+    if key in a_dictionary:
+            del a_dictionary[key]
+            return (a_dictionary)
