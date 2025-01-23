f119f9625e8bfaabd7e498a4e12d5a5cd9b53da2
Mame-Penda <mamependasadio1@gmail.com>
2025-01-23 22:31:32 +0100

    fixed square files

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    for row in new_matrix:
-        for i in range(len(row)):
-            row[i] = row[i] ** 2
-            return new_matrix
+    """
+    Computes the square value of all integers in a matrix.
+
+    Args:
+        matrix (list of lists): A 2D array of integers.
+
+    Returns:
+        list of lists: A new matrix with squared values, same size as the input matrix.
+    """
+    # Utiliser une compréhension de liste pour créer une nouvelle matrice avec les carrés des valeurs
+    return [[x ** 2 for x in row] for row in matrix]
