cb5dd20862a2fe6f420dd000660d1c083b42c533
Mame-Penda <mamependasadio1@gmail.com>
2025-01-23 22:33:56 +0100

    fixed error files

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -7,7 +7,5 @@ def square_matrix_simple(matrix=[]):
         matrix (list of lists): A 2D array of integers.
 
     Returns:
-        list of lists: A new matrix with squared values, same size as the input matrix.
     """
-    # Utiliser une compréhension de liste pour créer une nouvelle matrice avec les carrés des valeurs
     return [[x ** 2 for x in row] for row in matrix]
