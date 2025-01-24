99c6cbec18b03b046ade1afee4a102a7c5886867
Mame-Penda <mamependasadio1@gmail.com>
2025-01-24 19:33:04 +0100

    modified code of adding files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,8 +7,21 @@ or floats before performing the addition.
 
 
 def add_integer(a, b=98):
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+        int: The sum of the two numbers as an integer.
+
+    Raises:
+        TypeError: If either `a` or `b` is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    return int(a) + int(b)
