2121d09e95d35b89f73e11c51818f5f217b2df62
Mame-Penda <mamependasadio1@gmail.com>
2025-01-27 21:07:02 +0100

    class of square that defines private instance attribute:

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,49 +1,32 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class square:
-    """Class qui définit un carré avec des méthodes pour modifier la taille."""
+    """Represent a square."""
     def __init__(self, size=0):
-        """Initialize instance square.
+        """Initialize a new square.
 
         Args:
-        size (int): Size of square
-
-        Raises:
-        TypeError: if size is not integer.
-        ValueError: if size is negative.
+        size (int): Size of the new square.
         """
         self.size = size
 
         @property
         def size(self):
-            """ Recovered the size of square
-
-            Returns:
-            int: Size of actual square.
-            """
-            return self.__size
+            """ Recovered the size of square."""
+            return (self.__size)
 
         @size.setter
         def size(self, value):
-            """Define a size square after validation.
-
-            Args:
-            value (int): Nex size of square.
-
-            Raises:
-            TypeError: if value is not a integer.
-            ValueError: if value is negatif
-            """
+            """Define a size square after validation."""
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
         self.__size = value
 
 
 def area(self):
-    """calcul and return area of square.
-
-    Returns:
-    int: Area of square.
-    """
-    return self.__size ** 2
+    """calcul and return area of square."""
+    return (self.__size * self.__size)
