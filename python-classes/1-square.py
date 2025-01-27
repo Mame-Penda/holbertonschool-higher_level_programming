5b4b9d668c6c37a29f680e0144467e19fde8164d
Mame-Penda <mamependasadio1@gmail.com>
2025-01-27 16:04:27 +0100

    size of the square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class square:
+    """ Define the size of the square."""
+    def __init__(self, size):
+        """ Initialize square with private attribut.
+
+        Args:
+        size (int): size of square.
+        """
+        self.__size = size
