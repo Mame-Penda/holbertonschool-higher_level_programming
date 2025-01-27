ef4c1b09023da09c15e92c5489f2860c8f9ffda7
Mame-Penda <mamependasadio1@gmail.com>
2025-01-27 16:54:27 +0100

    class that define the area of square

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class square:
+    """Class qui définit un carré avec une taille validée."""
+    def __init__(self, size=0):
+        """Initialize square.
+
+        Args:
+        size (int): Size of square
+
+        Raises:
+        TypeError: if size is not integer.
+        ValueError: if size is negative.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+def area(self):
+    """calcul and return area of square.
+
+    Returns:
+    int: Area of square.
+    """
+    return self.__size ** 2
