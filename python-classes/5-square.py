cbb3c54474e605990cc27e99a8f608c694af3504
Mame-Penda <mamependasadio1@gmail.com>
2025-01-28 12:08:25 +0100

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define a class square."""
+"""Define a class Square."""
 
 
 class square:
-    """Represent a square."""
+    """Represent a Square."""
     def __init__(self, size=0):
         """Initialize a new square.
 
