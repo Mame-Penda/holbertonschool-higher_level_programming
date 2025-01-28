35829bcfb62b76e3ffff0939f518ce56d66ebd5f
Mame-Penda <mamependasadio1@gmail.com>
2025-01-28 16:01:15 +0100

    class square access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+""" This module creates a Square Class """
 
 
-class square:
-    """Represent a Square."""
+class Square:
+    """
+    Square class with size validation
+    Area method to calculate the area of the square
+    Property size to retrieve the size of the square
+    Property position to retrieve the position of the square
+    Size setter to set the size of the square
+    Property position setter to set the position of the square
+    Print method to print the square
+    """
     def __init__(self, size=0):
-        self.__size = size
-        """Initialize a new Square.
-
-        Args:
-        size (int): Size of the new square.
-
-        """
-        __size = None
         self.size = size
 
-        @property
-        def size(self):
-            """Get/set the current size of the square."""
-            return (self.__size)
-
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-        self.__size = value
-
-
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
