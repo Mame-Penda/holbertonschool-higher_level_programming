e47e6bce08f06b4464d25f526612b3c1e50b94bb
Mame-Penda <mamependasadio1@gmail.com>
2025-01-30 10:30:37 +0100

    fixed error that defines  String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,6 +47,7 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
+            print()
             return ""
         str = ""
         for i in range(self.__height):
