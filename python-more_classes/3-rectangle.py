595409b2258a48d968000b464f975310b0b582ba
Mame-Penda <mamependasadio1@gmail.com>
2025-01-30 10:37:13 +0100

    class that defines  String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,6 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            print()
             return ""
         str = ""
         for i in range(self.__height):
