5531007d4a803ea495161f18a7e4227153e9120e
Mame-Penda <mamependasadio1@gmail.com>
2025-02-03 16:58:31 +0100

    class that defines class BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class_checking function."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
