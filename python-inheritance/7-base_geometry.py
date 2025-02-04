2ab328761ee5e2e67f6bfda7eb9291747e368c45
Mame-Penda <mamependasadio1@gmail.com>
2025-02-04 16:44:52 +0100

    class of base geometry  that defines Integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,18 +7,11 @@ class BaseGeometry:
 
     def area(self):
         """Method that raises an Exception"""
-        raise Exception("area() is not implemented")
+        if self:
+            raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Method that validates value.
-
-        Args:
-        name (str): The name of the parameter.
-        value (int): The parameter to validate.
-        Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is <= 0.
-        """
+        """Method that validates value."""
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
