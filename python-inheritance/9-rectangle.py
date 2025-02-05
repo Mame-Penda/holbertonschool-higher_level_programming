45ebcb06b04c31ceb5d425093e549d84bfe6f775
Mame-Penda <mamependasadio1@gmail.com>
2025-02-05 13:50:26 +0100

    modified full rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,7 +2,7 @@
 """Module for the BaseGeometry class."""
 
 
-Rectangle = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
