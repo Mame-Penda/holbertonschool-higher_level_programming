36732167fdf25d7c4b187613d4b39d029938593a
Mame-Penda <mamependasadio1@gmail.com>
2025-02-07 18:20:30 +0100

    The Enigmatic FlyingFish - Exploring Multiple Inheritance

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,23 +1,27 @@
-#!/usr/bin/pyton3
-
+#!/usr/bin/python3
 
 class Fish():
     def swim(self):
         print("The fish is swimming")
 
-        def habitat(self):
-            print("The lives in water")
+    def habitat(self):
+        print("The fish lives in water")
 
-            class Bird():
-                def fly(self):
-                    print("The bird lives in the sky")
 
-                    class Flyingfish(Fish, Bird):
-                        def fly(self):
-                            print("The flying fish is soaring!")
+class Bird():
+    def fly(self):
+        print("The bird is flying")
 
-                            def swim(self):
-                                print("The flying fish is swimming!")
+    def habitat(self):
+        print("The bird lives in the sky")
 
-                                def habitat(self):
-                                    print("The flying fish lives both in water and sky!")
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
