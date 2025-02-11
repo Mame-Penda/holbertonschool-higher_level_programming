b68df1ddc89ff6f29b7d69c21eb278c33f0ecc1c
Mame-Penda <mamependasadio1@gmail.com>
2025-02-11 15:15:34 +0100

    function that defines from string to json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/pytho3
-"""Defines a string-to-JSON function."""
+"""Defines for from string-to-JSON function."""
 import json
 
 
 def from_json_string(my_str):
-    """Return the python object representation of a JSON string."""
+    """returns the python object representation of a JSON string."""
     return json.loads(my_str)
