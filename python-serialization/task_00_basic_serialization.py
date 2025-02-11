d689226320bca2bb981b196b746d28cd946885e5
Mame-Penda <mamependasadio1@gmail.com>
2025-02-11 15:35:47 +0100

    serialization module that adds the functionality to serialize a Python dictionary

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines serialize_and_save_to_file and load_and_deserialize methods."""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize a python dictionary to a JSON file."""
+    with open(filename, 'w+', encoding="utf-8") as f:
+        data = json.dumps(data)
+        f.write(data)
+        pass
+
+
+def load_and_deserialize(filename):
+    """deserialize the JSON file to recreate the python Dinctionary."""
+    with open(filename, 'r', encoding="utf-8") as f:
+        data = f.read()
