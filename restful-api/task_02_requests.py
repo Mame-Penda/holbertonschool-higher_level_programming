f17deb26e1eaa30a36a53e67cd4fed19aaed72d5
Mame-Penda <mamependasadio1@gmail.com>
2025-02-19 10:31:49 +0100

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,18 +19,10 @@ def fetch_and_save_posts():
     """Fetches posts, structures the data, and saves it to a csv file."""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
-        posts = response.json()
-        data = [{"id": post["id"], "title": post["title"], "body":
-                 post["body"]} for post in posts]
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            fielnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fielnames=fielnames)
 
-    with open("posts.csv", mode="w", newline="", encoding="utf-8") as csvfile:
-        fielnames = ["id", "title", "body"]
-        writer = csv.DictWriter(csvfile, fielnames=fielnames)
-
-        writer.writeheader()
-        writer.writerows(data)
-        print("Data saved to posts.csv")
-
-    if __name__ == "__main__":
-        fetch_and_print_posts()
-        fetch_and_save_posts()
+            writer.writeheader()
+            for post in response.json():
+                writer.writerows([{key: post[key] for key in fielnames}])
