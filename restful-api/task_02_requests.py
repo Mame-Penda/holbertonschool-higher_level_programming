b7cc02fe1e6a649493613d49a5be319fcc7af4f8
Mame-Penda <mamependasadio1@gmail.com>
2025-02-19 20:43:07 +0100

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Consuming and processing data from an API using Python."""
 import requests
-import json
 import csv
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
+statuscode = response.status_code
 
 
 def fetch_and_print_posts():
     """Fetches posts from JSONPlaceholder and prints the titles."""
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
@@ -17,12 +17,11 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     """Fetches posts, structures the data, and saves it to a csv file."""
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
         with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
-            fielnames = ["id", "title", "body"]
-            writer = csv.DictWriter(csvfile, fielnames=fielnames)
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writeheader()
             for post in response.json():
-                writer.writerows([{key: post[key] for key in fielnames}])
+                writer.writerows([{key: post[key] for key in fieldnames}])
