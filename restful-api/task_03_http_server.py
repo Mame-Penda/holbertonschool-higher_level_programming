51b6aaf417548a77aad6efbb5aaaabb56fcd3d1c
Mame-Penda <mamependasadio1@gmail.com>
2025-02-20 13:54:47 +0100

    modif

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,5 +1,5 @@
 #!usr/bin/python3
-"""Develop a simple API using Python with the `http.server` modulea
+"""Develop a simple API using Python with the `http.server` module
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
