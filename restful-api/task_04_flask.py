e97c9c8ce906022491159caf756400b9eb772d2d
Mame-Penda <mamependasadio1@gmail.com>
2025-02-20 20:42:58 +0100

    handle error of the code

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Develop a Simple API using Python with Flask."""
-from flask import Flask, request, jsonify
+"""
+Un script Python basique pour récupérer des posts de
+JSONPlaceholder en utilisant requests
+"""
+from flask import Flask, jsonify, request
+"""importation"""
 
 
 app = Flask(__name__)
@@ -10,40 +14,40 @@ app = Flask(__name__)
 users = {}
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    """Return str"""
+    """home"""
     return "Welcome to the Flask API!"
 
 
-@app.route('/data')
-def list_users():
-    """Return list of users in data path"""
+@app.route("/data")
+def data():
+    """Return a list of all usernames in the 'users' dictionary"""
     return jsonify(list(users.keys()))
 
 
-@app.route('/status')
-def send_status():
-    """Return str in status path"""
-    return "ok"
+@app.route("/status")
+def status():
+    """status"""
+    return "OK"
 
 
-@app.route('/users/<username>')
+@app.route("/users/<username>")
 def get_user(username):
-    """Return a user data"""
+    """Check if the user exists in the dictionary"""
     user = users.get(username)
     if user:
-        return jsonify(users)
+        return jsonify(user)
     else:
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    """Return user in users."""
+    """Parse the incoming JSON data"""
     user_data = request.get_json()
     if not user_data or "username" not in user_data:
-        return jsonify({"error": "USername is required"}), 400
+        return jsonify({"error": "Username is required"}), 400
 
     username = user_data["username"]
     users[username] = user_data
