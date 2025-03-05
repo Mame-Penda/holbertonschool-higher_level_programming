641e3e0af2cd506a0ec139e9f2f122e41d3df839
Mame-Penda <mamependasadio1@gmail.com>
2025-03-05 13:26:06 +0100

    script that creates a table second_table in the database hbtn_0c_0.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,5 @@
 -- script that creates a table second_table in the database hbtn_0c_0.
-CREATE TABLE if NOT EXISTS second_table (
-    id INT, 
-    name VARCHAR(256), 
-    score INT
-    );
-
+CREATE TABLE if NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES
 (1, 'John', 10),
