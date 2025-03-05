be8233fc17c0c59c4bba155796c87ba2a0ba99ef
Mame-Penda <mamependasadio1@gmail.com>
2025-03-05 15:37:22 +0100

    script that creates the database hbtn_0d_usa

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE TABLE if NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    UNIQUE(id)
+    UNIQUE(id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
