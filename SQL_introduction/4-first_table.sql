6327ab63489d82f6425de3be5815d4f9af6575a0
Mame-Penda <mamependasadio1@gmail.com>
2025-03-05 12:49:02 +0100

    script that creates a table called first_table in database in MySQL server.

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- script that creates a table called first_table in database in MySQL server.
-CREATE TABLES IF NOT EXISTS first_table(id INT, name VARCHAR(256))
+CREATE TABLE if NOT EXISTS first_table(id INT, name VARCHAR(256))
