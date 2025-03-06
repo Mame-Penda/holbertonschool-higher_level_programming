125bfde750f9a275957a51cd149a49c0e6d81750
Mame-Penda <mamependasadio1@gmail.com>
2025-03-06 15:40:14 +0100

    script that lists all Comedy shows in the database hbtn_0d_tvshows.

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+--  script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_show.title
+FROM tv_shows
+INNER JOIN tv_shows ON tv_show.id = tv_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title;
+
