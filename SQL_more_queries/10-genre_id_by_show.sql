df1a369ef2c7af760fd8dc78d488bf202ed3f04b
Mame-Penda <mamependasadio1@gmail.com>
2025-03-06 13:42:25 +0100

    lists all shows contained in hbtn_0d_tvshows -- that have at least one genre linked

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
 JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
