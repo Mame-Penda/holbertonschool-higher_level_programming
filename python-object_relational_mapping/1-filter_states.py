d2bbe0543cb58734e23b6a62feafab576b27a54f
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 15:22:25 +0100

    script that lists all states with a name starting with N (upper N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N"""
+"""script that lists all states with a name starting with N (upper N)"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Connexion à la base de données
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3],
+        db=sys.argv[3],
         port=3306
     )
-    curs = db.cursor()
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
-    row = curs.fetchall()
 
-    for row in curs:
-        if row[1][0] == 'N':
-            print(row)
-    curs.close()
+    cur = db.cursor()
+
+    # Exécuter la requête pour récupérer tous les états triés par id
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+
+    # Récupérer et afficher les résultats
+    for state in cur.fetchall():
+        print(state)
+
+    # Fermer le curseur et la connexion à la base de données
+    cur.close()
     db.close()
