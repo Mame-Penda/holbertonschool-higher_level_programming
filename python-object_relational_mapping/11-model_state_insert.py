8ed3d56981d002e7e8c20832aa86170e52305e4c
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 20:43:06 +0100

    script that adds the State object “Louisiana” to the database

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""script that adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+"""script that adds the State object “Louisiana” to the database"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1],
                                    sys.argv[2],
