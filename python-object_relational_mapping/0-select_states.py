e3b152bb30f2a4e025f022b30d4e475b9ad94ae9
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 14:53:17 +0100

    Module that query USA States from a database table

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""Module that query USA States from a database table"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    conn = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3],
-        port=3306
-    )
-    curs = db.cursor()
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
-    row = curs.fetchall()
-
-    for row in row:
+        db=sys.argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    state = sys.argv[4]
+    cur.execute("SELECT * FROM states \
+        WHERE name = %s ORDER BY id ASC", (state,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    curs.close()
-    db.close()
+    cur.close()
+    conn.close()
