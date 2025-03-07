0e91eb1e417b6d519919935637e244e05612ce34
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 21:49:30 +0100

    deletes all State object

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""Script that lists all City objects from the database hbtn_0e_14_usa"""
+"""deletes all State object"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(engine)
     session = Session()
 
-    cities = session.query(City).join(State).order_by(City.id).all()
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    cities = (
+        session.query(City, State)
+        .filter(City.state_id == State.id)
+        .order_by(City.id).all()
+    )
 
+    for cities, state in cities:
+        print("{}: ({}) {}".format(state.name, cities.id, cities.name))
     session.close()
