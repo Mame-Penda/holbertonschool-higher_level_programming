711e57eb61452585b5f39a87d970cf771bce9c95
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 15:47:10 +0100

    script that takes in an argument and displays all values in the states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connexion à la base de données
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    # Exécuter la requête pour récupérer tous les états triés par id
+    query = (
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC;"
+        ).format(sys.argv[4])
+
+    cur.execute(query)
+    # Récupérer et afficher les résultats
+    for state in cur.fetchall():
+        print(state)
+
+    # Fermer le curseur et la connexion à la base de données
+    cur.close()
+    db.close()
