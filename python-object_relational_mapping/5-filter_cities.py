be92d4c32e666c2152ff65c7c42ce5329a38749b
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 17:30:38 +0100

    list all cities of states with the name of states

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument and lists"""
-
+"""list all cities of states with the name of states"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connexion à la base de données
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
+        password=sys.argv[2],
+        database=sys.argv[3],
         port=3306
     )
-
-    cur = db.cursor()
-
-    # requête SQL pour recupérer les villes avec leur état associé
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC, (sys.argv[4],)
-        """
-    cur.execute(query)
-
-    # Récupérer et afficher les résultats
-    for cities in cur.fetchall():
-        print(", ".join(city[0] for city in cities))
-
-    # Fermer le curseur et la connexion à la base de données
-    cur.close()
+    curs = db.cursor()
+    curs.execute("SELECT cities.name "
+                 "FROM cities "
+                 "JOIN states ON cities.state_id = states.id "
+                 "WHERE states.name = %s"
+                 "ORDER BY cities.id ASC", (sys.argv[4],))
+    row = curs.fetchall()
+    print(", ".join([city[0] for city in row]))
+    curs.close()
     db.close()
