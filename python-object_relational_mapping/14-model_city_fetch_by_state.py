f9dc0e873b319601ab42c87f25677faa81d75a71
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 21:32:47 +0100

    Script that lists all city objets from the database

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,8 +19,11 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)  # Ajout explicite de bind=
     session = Session()
 
-    cities = session.query(City).join(
-        State).order_by(City.id).all()
+    cities = (
+        session.query(City, State)
+        .filter(City.state_id == State.id)
+        .order_by(City.id).all()
+    )
     for city in cities:
         print(f"{city.state.name}: ({city.id}) {city.name}")
 
