d4b14b9582ca45b69d6b3f5bff7e8aeaa798cd5a
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 14:05:48 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N"""
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
@@ -7,12 +8,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
-        passwd=sys.argv[2],
+        password=sys.argv[2],
         database=sys.argv[3],
         port=3306
     )
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
     row = curs.fetchall()
 
     for row in curs:
