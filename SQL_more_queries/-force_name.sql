2f5a616d860e5b7e1631c26a24671ae29bb6827d
Mame-Penda <mamependasadio1@gmail.com>
2025-03-07 21:35:07 +0100

    Defines a State and City class that link to the MySQL

--- SQL_more_queries/-force_name.sql
+++
@@ -1,5 +0,0 @@
--- creates the table force_name
-CREATE TABLE if NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-)
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Defines a State and City class that link to the MySQL tables"""
-from sqlalchemy import Column, Integer, String, Foreignkey, create_engine
+"""
+Defines a State and City class that link to the MySQL tables 'states' and 'cities'.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 # Create instance of declarative_base
 Base = declarative_base()
-
 
 class State(Base):
     """
@@ -15,11 +16,11 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    class City(Base):
-        """
-        City class that represents the 'cities' table.
-        """
+class City(Base):
+    """
+    City class that represents the 'cities' table.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, Foreignkey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
