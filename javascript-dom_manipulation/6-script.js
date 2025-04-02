728eaf8692d32b31ada8d37ffc0afd3d46e7dbb2
Mame-Penda <mamependasadio1@gmail.com>
2025-04-02 21:01:32 +0200

    script

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,20 @@
+async function getCharName() {
+    try {
+        const response = await fetch('https://swapi-api.hbtn.io/api/people/5/?format=json');
+        if (!response.ok) {
+            throw newError(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json()
+        if (data && data.name) {
+            return data.name;
+        } else {
+            throw new Error('Data not found');
+        }
+    } catch (err) {
+        console.error(err)
+    } 
+    const character = document = document.getElementById('character');
+    getCharName().then((name) => {
+        character.innerHTML = name;
+    })
+}
