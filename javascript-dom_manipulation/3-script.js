9e4748edb1afc7cf8f57156fcb948d148571e979
Mame-Penda <mamependasadio1@gmail.com>
2025-04-03 14:58:41 +0200

    script

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -2,5 +2,5 @@ const toggleHeader = document.getElementById("toggle_header");
 toggleHeader.addEventListener('click', function() {
     const header = document.querySelector('header');
     header.classList.toggle('green');
-    hearder.classList.toggle('red');
+    header.classList.toggle('red');
 })
